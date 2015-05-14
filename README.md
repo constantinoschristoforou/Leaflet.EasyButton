14a2af5b7bd8604116f4903608d1e8efa04f4d59
Dan <rocks.in.the.cloud@gmail.com>
2015-05-14 09:58:06 -0700

    refactored and revised into v1

--- README.md
+++ README.md
@@ -2,4 +2,17 @@
 
 Relevant documentation and implementation in the [Demo][].
 
+Changes from the original:
+  * The Name
+    * now: `L.Control.EasyButton ` (no trailing 's')
+    * was: `L.Control.EasyButtons` (trailing 's')
+  * function signature
+    * now:
+      * `<icon:string>`,`<callback:function>`
+      * `<options:object>`
+    * was:
+      * `<icon:string>`,`<callback:function>`,`<map:leaflet-map>`,`<button-title:string>`,`<button-id:string>`
+
+  * added features
+
 [Demo]:http://cliffcloud.github.io/Leaflet.EasyButton/
--- easy-button.js
+++ easy-button.js
@@ -1,57 +1,91 @@
-L.Control.EasyButtons = L.Control.extend({
+L.Control.EasyButton = L.Control.extend({
     options: {
-        position: 'topleft',
-        title: '',
-        intendedIcon: 'fa-circle-o'
+        position:  'topleft',
+        callback:  function(){ console.log( 'default in L.easyButton' )},
+        id:        null,
+        title:     '',
+        icon:      'fa-circle fa-lg',
+        builtIcon: null
+    },
+
+    initialize: function(uno, dos, tres){
+        // the factory tacks on `undefined`
+        // with some of the shorter signatures
+        // so
+        while( arguments[arguments.length - 1] === undefined ){
+          Array.pop.call(null, arguments);
+        }
+
+        if( arguments[arguments.length-1] && typeof arguments[arguments.length-1] === "object" ){
+            L.Util.setOptions( this, arguments[arguments.length-1] );
+        }
+
+        // if the uno argument is a string, set it
+        if( uno && typeof uno  === "string"){
+          this.options.icon = uno;
+        }
+
+        // if the dos argument is a function, set it
+        if( dos && typeof dos === 'function'){
+          L.Util.setOptions( this, { callback: dos});
+        }
     },
 
     onAdd: function () {
         var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control');
 
         this.link = L.DomUtil.create('a', 'leaflet-bar-part', container);
-        this._addImage()
+        this._buildIcon()
         this.link.href = '#';
+        this.link.title = this.options.title;
 
         L.DomEvent.on(this.link, 'click', this._click, this);
-        this.link.title = this.options.title;
 
         return container;
     },
 
-    intendedFunction: function(){ alert('no function selected');},
-
     _click: function (e) {
         L.DomEvent.stopPropagation(e);
         L.DomEvent.preventDefault(e);
-        this.intendedFunction();
+        this.options.callback();
     },
 
-    _addImage: function () {
-        var extraClasses = this.options.intendedIcon.lastIndexOf('fa', 0) === 0 ? ' fa fa-lg' : ' glyphicon';
+    _buildIcon: function () {
 
-        var icon = L.DomUtil.create('i', this.options.intendedIcon + extraClasses, this.link);
-        icon.id = this.options.id;
-    }
+      // does this look like html? (i.e. not a class)
+      if( this.options.icon.match(/[&;=<>"']/) ){
+
+        // if so, the user should have put in html
+        // so move forward as such
+        this.options.builtIcon = this.options.icon;
+
+      } else { // figure out what the user wants
+          this.options.icon = this.options.icon.trim();
+          this.options.builtIcon = L.DomUtil.create('span', '', this.link);
+
+          if( this.options.icon.indexOf('fa-') === 0 ){
+            L.DomUtil.addClass(this.options.builtIcon, "fa fa-lg "  + this.options.icon)
+
+          } else if ( this.options.icon.indexOf('glyphicon-') === 0 ) {
+            L.DomUtil.addClass(this.options.builtIcon, "glyphicon " + this.options.icon)
+
+          } else {
+            L.DomUtil.addClass(this.options.builtIcon, /*rollwithit*/ this.options.icon)
+          }
+
+          this.options.id && (this.options.builtIcon.id = this.options.id);
+
+          // make this a string so that it's easy to set innerHTML below
+          this.options.builtIcon = this.options.builtIcon.outerHTML;
+      }
+
+      // put the builtIcon inside the button
+      this.link.innerHTML = this.options.builtIcon;
+      return;
+    },
 });
 
-L.easyButton = function( btnIcon , btnFunction , btnTitle , btnMap , btnId) {
-  var newControl = new L.Control.EasyButtons();
-
-  if (btnIcon) newControl.options.intendedIcon = btnIcon;
-  if (btnId) newControl.options.id = btnId;
-
-  if ( typeof btnFunction === 'function'){
-    newControl.intendedFunction = btnFunction;
-  }
-
-  if (btnTitle) newControl.options.title = btnTitle;
-
-  if ( btnMap === '' ){
-    // skip auto addition
-  } else if ( btnMap ) {
-    btnMap.addControl(newControl);
-  } else {
-    map.addControl(newControl);
-  }
+L.easyButton = function( first, second, third ) {
+  var newControl = new L.Control.EasyButton( first, second, third );
   return newControl;
 };
